72c416c866f9cbc792c07d0922152fbf2d39ceb9
Matteovanypersele <114217424+Matteovanypersele@users.noreply.github.com>
2024-11-12 20:49:56 +0100

    Add files via upload

---
+++ Interfacestreamlit/app4.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+
+@st.cache_resource
+def load_model():
+    model_name = "Mvanypersele/F5smallGSM8trained"
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    return model, tokenizer
+
+model, tokenizer = load_model()
+
+def generate_answer(question):
+    inputs = tokenizer(question, return_tensors="pt", truncation=True, padding=True)
+    outputs = model.generate(**inputs, max_length=150, num_beams=4, early_stopping=True)
+    answer = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return answer
+
+st.title("Assistant Pédagogique en Mathématiques Appliquées à l'Économie")
+st.write("Posez vos questions et obtenez des réponses détaillées.")
+
+question = st.text_input("Votre question :")
+
+if st.button("Obtenir la réponse"):
+    if question:
+        with st.spinner("L'Assistant réfléchit..."):
+            answer = generate_answer(question)
+        st.write("Réponse : ", answer)
+    else:
+        st.warning("Veuillez poser une question.")
